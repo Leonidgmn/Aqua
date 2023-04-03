f0dbdd68c611a91143c0c4fa4650213417031b49
Leonid Home Laptop <leonidgmn@gmail.com>
2023-04-03 22:29:25 +0300

    add forms

--- aqua/src/App.js
+++ aqua/src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+
+import { Formik, Field, Form } from "formik";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Formik
+        initialValues={{ firstName: "", lastName: "", email: "" }}
+        onSubmit={(values) => {
+          console.log(`${values.firstName} ${values.lastName} - ${values.email}`)
+        }}
+      >
+        {props => (
+          <Form>
+            <Field name="firstName" type="text" placeholder="First Name"/>
+            <Field name="lastName" type="text" placeholder="Last Name"/>
+            <Field name="email" type="email" placeholder="Email" value={props.values.email}/>
+            <button type="submit">Submit</button>
+        </Form>
+        )}
+        
+      </Formik>
+      <Formik
+        initialValues={{ phone: "", description: "", color: ""}}
+        onSubmit={(values) => {
+          console.log(`${values.phone} ${values.description}; color: ${values.color}`)
+        }}
+      >
+        {props => (
+          <Form>
+            <Field name="phone" type="number" placeholder="+380971234567" value={props.values.phone}/>
+            <Field name="description" type="text" placeholder="text"/>
+            <Field as="select" name="color">
+             <option value="red">Red</option>
+             <option value="green">Green</option>
+             <option value="blue">Blue</option>
+           </Field>
+            <button type="submit">Submit</button>
+            
+          </Form>
+        )}
+      </Formik>
+
+
+    </>
   );
 }
 
